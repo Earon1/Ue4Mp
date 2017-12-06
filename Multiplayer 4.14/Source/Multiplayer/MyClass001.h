ebbc40cd615c6d1dadfc376bd23861240b309319
Aron Gaspic <arongaspic@gmail.com>
2017-12-06 19:57:46 +0100

    1GB
    first try

---
+++ Multiplayer 4.14/Source/Multiplayer/MyClass001.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class MULTIPLAYER_API MyClass001
+{
+public:
+	MyClass001();
+	~MyClass001();
+};
